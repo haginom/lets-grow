ed25cf03192546b4f81d8d95c12ffc95c81cc1f7
sakireid <59439482+haginom@users.noreply.github.com>
2023-03-31 14:43:02 +0200

    chaning mux video css

--- web/src/components/video-page.js
+++ web/src/components/video-page.js
@@ -2,24 +2,33 @@ import React from "react"
 import Layout from "./layout/layout"
 import Seo from "./seo"
 import MuxPlayer from "@mux/mux-player-react"
+import styled from "styled-components"
 
 const Video = ({ song, videoCall, ...props }) => {
-  const { filename, assetId, videoResources } = props
+  const { name, assetId, videoResources } = props
   let playbackId = videoResources?.video?.asset?.playbackId
 
   return (
     <Layout portal>
-      <Seo title="" />
-      <MuxPlayer
-        streamType="on-demand"
-        playbackId={playbackId}
-        metadata={{
-          video_id: { assetId },
-          video_title: { filename },
-        }}
-      />
+      <Seo title={`${name}`} />
+      <StyledContainer>
+        <MuxPlayer
+          style={{ aspectRatio: "16 / 9" }}
+          streamType="on-demand"
+          playbackId={playbackId}
+          metadata={{
+            video_id: { assetId },
+            video_title: `${name}`,
+          }}
+        />
+      </StyledContainer>
     </Layout>
   )
 }
 
 export default Video
+
+const StyledContainer = styled.div`
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+`

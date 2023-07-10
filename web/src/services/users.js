c8c263feb5efc800f26e0f4b0da6fd291adc7f9b
Saki Reid <saki.reid@gmail.com>
2023-07-10 07:56:05 +0200

    adding users

--- web/src/services/users.js
+++ web/src/services/users.js
@@ -1,4 +1,9 @@
 export const users = [
   { username: "mimifaulks", password: "Swede8562", trial: false },
-  { username: "joworthy", password: "Broccoli822", trial: true },
+  { username: "joworthy", password: "Broccoli822", trial: false },
+  { username: "lisatownsend", password: "Celeriac9725", trial: false },
+  { username: "sarahbarker", password: "Radish506", trial: false },
+  { username: "gailclayton", password: "Beetroot864", trial: false },
+  { username: "natashawillenbrook", password: "Asparagus1246", trial: false },
+  { username: "starharford", password: "Cauliflower97", trial: false },
 ]
